c87a46bc82156c3302c0930ea466a4af9965dec9
Zhassyn Zhalynuly <zhasynz05@mail.ru>
2026-09-25 20:58:38 +0500

    feat: implement DynamicArray add

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,3 +1,31 @@
 public class DynamicArray {
+    private int[] data;
+    private int size;
 
+    public DynamicArray() {
+        data = new int[10];
+        size = 0;
+    }
+
+    public void add(int value) {
+        ensureCapacity();
+        data[size] = value;
+        size++;
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            int[] newData = new int[data.length * 2];
+
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+
+            data = newData;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,3 +1,11 @@
 public class Tests {
+    public static void main(String[] args) {
+        DynamicArray array = new DynamicArray();
 
+        array.add(10);
+        array.add(20);
+        array.add(30);
+
+        System.out.println(array.size());
+    }
 }
